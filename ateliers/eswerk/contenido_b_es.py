4021374bc3df82ed4ac0ee8bbc645988324fe492
Etienne Masse <35309944+Etienne1105@users.noreply.github.com>
2026-07-11 10:02:05 +0200

    feat(atelier): contenido_b_es.py — forge ES + skill Globetrotter

---
+++ ateliers/eswerk/contenido_b_es.py
@@ -0,0 +1,188 @@
+# -*- coding: utf-8 -*-
+# Contenido ES — moitié B : sections 5-9, despedida (§11), kolophon, geheim (La Cremà).
+from contenido_a_es import T
+
+FICHES_B = []
+
+FICHES_B.append(T("p5-1", 5, 1, 2, "Para beber &mdash; l&rsquo;horchata, blanche et sacr&eacute;e", "&amp; fartons",
+ '<p><span class="lettrine le">L</span>&rsquo;<strong lang="es">horchata de chufa</strong> est le lait '
+ 'v&eacute;g&eacute;tal du souchet, cultiv&eacute; dans seize villages de l&rsquo;Horta Nord sous '
+ 'appellation d&rsquo;origine (capitale&nbsp;: <strong>Alboraya</strong>). La vraie est blanc cass&eacute;, '
+ 'go&ucirc;t de noisette, <strong>jamais de gla&ccedil;ons</strong> &mdash; ils la noient. On y trempe des '
+ '<strong lang="es">fartons</strong>, brioches longues glac&eacute;es au sucre, et c&rsquo;est le go&ucirc;ter '
+ 'valencien absolu.</p>'
+ '<p>Les temples&nbsp;: <strong>Horchater&iacute;a Daniel</strong> (Alboraya&nbsp;; comptoirs au Mercado '
+ 'de Col&oacute;n et Plaza de la Reina) et <strong>Santa Catalina</strong>, deux si&egrave;cles d&rsquo;azulejos '
+ 'pr&egrave;s de la cath&eacute;drale. Cousin d&rsquo;hiver&nbsp;: le <em lang="es">cremaet</em> &mdash; '
+ 'liqueur flamb&eacute;e, caf&eacute;, mousse, servi en couches &mdash; et le <em>caf&eacute; del temps</em>, '
+ 'expresso avec verre de gla&ccedil;ons &agrave; c&ocirc;t&eacute;.</p>'
+ '<div class="merksatz"><p>Horchata jaun&acirc;tre ou granuleuse = industrielle. La bonne se boit '
+ 'le jour m&ecirc;me, du comptoir, debout si possible.</p></div>'))
+
+FICHES_B.append(T("p5-2", 5, 2, 2, "Para beber &mdash; l&rsquo;agua de Valencia, la tra&icirc;tresse", "&amp; les vins",
+ '<p><span class="lettrine le">I</span>nvent&eacute;e en 1959 au Caf&eacute; Madrid par Constante Gil, '
+ 'l&rsquo;<strong lang="es">Agua de Valencia</strong> marie jus d&rsquo;orange local, cava, gin et vodka '
+ '&mdash; servie en <strong>pichet partag&eacute;</strong> (~15&ndash;20&nbsp;&euro;), fra&icirc;che, sucr&eacute;e, '
+ 'et beaucoup plus forte qu&rsquo;elle n&rsquo;en a l&rsquo;air (~15&deg;). &Agrave; go&ucirc;ter au '
+ '<strong>Caf&eacute; de las Horas</strong> (d&eacute;cor baroque fou) ou au Caf&eacute; Madrid lui-m&ecirc;me.</p>'
+ '<p>C&ocirc;t&eacute; bi&egrave;res artisanales&nbsp;: <strong>Tyris</strong> (taproom pr&egrave;s de la '
+ 'Lonja), Zeta, et l&rsquo;espi&egrave;gle <em>La Socarrada</em>. C&ocirc;t&eacute; vins&nbsp;: '
+ '<strong>DO Val&egrave;ncia</strong> pour les blancs (Merseguera, Moscatel) et '
+ '<strong>DO Utiel-Requena</strong> pour les rouges du c&eacute;page <strong>Bobal</strong> &mdash; '
+ 'et ses cavas, qui font justement p&eacute;tiller l&rsquo;agua de Valencia.</p>'
+ '<div class="achtung"><p>Un pichet d&rsquo;agua de Valencia &agrave; deux, tr&egrave;s bien. Un pichet '
+ 'chacune, et le Miguelete se met &agrave; tanguer.</p></div>'))
+
+FICHES_B.append(T("p6-1", 6, 1, 2, "La ciudad &mdash; la vieille pierre", "Ciutat Vella",
+ '<p><span class="lettrine le">T</span>out le centre se marche. La <strong>cath&eacute;drale</strong> '
+ 'abrite le <strong>Santo C&aacute;liz</strong> &mdash; la coupe que la tradition identifie au '
+ '<em>Graal</em> &mdash; et son clocher octogonal, le <strong>Miguelete</strong>, offre la ville '
+ 'enti&egrave;re pour 207 marches. &Agrave; c&ocirc;t&eacute;&nbsp;: <strong>San Nicol&aacute;s</strong>, '
+ '&laquo;&nbsp;Sixtine valencienne&nbsp;&raquo; aux fresques baroques renversantes.</p>'
+ '<p>La <strong>Lonja de la Seda</strong> (1482&ndash;1533), bourse de la soie au gothique civil '
+ 'class&eacute;e UNESCO, co&ucirc;te 2&nbsp;&euro; &mdash; <strong>gratuite le dimanche</strong>, comme les '
+ 'tours m&eacute;di&eacute;vales de <strong>Serranos</strong> et <strong>Quart</strong>. En face, le '
+ '<strong>Mercado Central</strong> (1928), cath&eacute;drale moderniste de la mangeaille&nbsp;: '
+ 'prends-y un <em>bocadillo de calamares</em> au comptoir.</p>'
+ '<div class="merksatz"><p>Le jeudi &agrave; midi, devant la porte des Ap&ocirc;tres&nbsp;: le '
+ '<strong>Tribunal de las Aguas</strong> juge les litiges d&rsquo;irrigation, &agrave; l&rsquo;oral, '
+ 'comme depuis mille ans. La plus vieille cour de justice d&rsquo;Europe, en plein air, gratuite.</p></div>'))
+
+FICHES_B.append(T("p6-2", 6, 2, 2, "La ciudad &mdash; le futur blanc de Calatrava", "&amp; les quartiers",
+ '<p><span class="lettrine le">D</span>ans l&rsquo;ancien lit du fleuve, la <strong>Cit&eacute; des Arts '
+ 'et des Sciences</strong> du Valencien Santiago Calatrava&nbsp;: l&rsquo;<strong>Oceanogr&agrave;fic</strong> '
+ '(plus grand aquarium d&rsquo;Europe, ~4h de visite), l&rsquo;&oelig;il g&eacute;ant de '
+ 'l&rsquo;<strong>Hemisf&egrave;ric</strong>, le mus&eacute;e des Sciences. Le combo trois sites '
+ '(~47,75&nbsp;&euro;, valable 1&ndash;3 jours) bat les billets s&eacute;par&eacute;s&nbsp;; r&eacute;serve en ligne.</p>'
+ '<p>Le tout se rejoint par les <strong>Jardins du Turia</strong>&nbsp;: 9&nbsp;km de parc dans le lit '
+ 'd&eacute;tourn&eacute; du fleuve &mdash; le plus beau boulevard cyclable d&rsquo;Espagne. C&ocirc;t&eacute; '
+ 'quartiers&nbsp;: loge &agrave; <strong>Ruzafa</strong> (le &laquo;&nbsp;Soho&nbsp;&raquo; gourmand, s&ucirc;r, '
+ 'vivant) ou en Ciutat Vella&nbsp;; flâne au <strong>Cabanyal</strong>, ancien village de p&ecirc;cheurs '
+ 'aux fa&ccedil;ades d&rsquo;azulejos, &agrave; deux pas de la Malvarrosa.</p>'
+ '<div class="achtung"><p>Les locations touristiques sont d&eacute;sormais r&eacute;glement&eacute;es&nbsp;: '
+ 'v&eacute;rifie que l&rsquo;appartement affiche un <strong>num&eacute;ro de licence</strong> avant de r&eacute;server.</p></div>'))
+
+FICHES_B.append(T("p7-1", 7, 1, 2, "La Albufera &mdash; la lagune m&egrave;re", "El Palmar",
+ '<p><span class="lettrine le">&Agrave;</span> vingt minutes au sud, le plus grand lac d&rsquo;eau douce '
+ 'd&rsquo;Espagne&nbsp;: l&rsquo;<strong>Albufera</strong>, s&eacute;par&eacute;e de la mer par une pin&egrave;de '
+ 'de dunes, cern&eacute;e de rizi&egrave;res &mdash; le berceau exact de la paella. On y va en bus '
+ '(lignes 24/25, ~1,50&nbsp;&euro;) jusqu&rsquo;au village de p&ecirc;cheurs d&rsquo;<strong>El Palmar</strong>, '
+ 'on y mange, puis on embarque.</p>'
+ '<p>La <strong>balade en barque traditionnelle</strong> au coucher du soleil est le moment le plus '
+ 'photog&eacute;nique du voyage&nbsp;: eau plate, h&eacute;rons, ciel orange sur les roseaux. '
+ 'Sp&eacute;cialit&eacute; du cru &agrave; oser&nbsp;: l&rsquo;<em lang="es">all i pebre</em>, rago&ucirc;t '
+ 'd&rsquo;anguille de la lagune &agrave; l&rsquo;ail et au paprika &mdash; bien meilleur que sa description.</p>'
+ '<div class="merksatz"><p>Plages sauvages&nbsp;: <strong>El Saler</strong> et <strong>La Devesa</strong>, '
+ 'dans le parc naturel &mdash; dunes et pins, sans b&eacute;ton. L&rsquo;anti-Benidorm.</p></div>'))
+
+FICHES_B.append(T("p7-2", 7, 2, 2, "La huerta &mdash; le jardin mill&eacute;naire", "l&rsquo;eau des Maures",
+ '<p><span class="lettrine le">L</span>a <strong>huerta</strong> est la ceinture mara&icirc;ch&egrave;re '
+ 'qui nourrit Valence depuis les <strong>Maures</strong>&nbsp;: leur r&eacute;seau d&rsquo;irrigation '
+ '&mdash; canaux, norias, tours de partage de l&rsquo;eau &mdash; fonctionne toujours, mille ans plus '
+ 'tard. C&rsquo;est lui que juge le Tribunal de las Aguas chaque jeudi. C&rsquo;est ici aussi que '
+ 'pousse la <strong>chufa</strong> d&rsquo;Alboraya, celle de ton horchata.</p>'
+ '<p>Les <strong>oranges de Valence</strong> ont fait la fortune et l&rsquo;image de la r&eacute;gion&nbsp;: '
+ 'en saison (novembre&ndash;avril), le parfum d&rsquo;<em lang="es">azahar</em> &mdash; la fleur '
+ 'd&rsquo;oranger &mdash; flotte sur la huerta. Le jus press&eacute; du march&eacute; n&rsquo;a rien &agrave; '
+ 'voir avec celui de l&rsquo;&eacute;picerie&nbsp;: profites-en chaque matin.</p>'
+ '<div class="achtung"><p>&Agrave; v&eacute;lo, la huerta d&rsquo;Alboraya se traverse en une heure douce '
+ 'depuis le centre&nbsp;: pistes plates, horchater&iacute;a au bout. La plus belle demi-journ&eacute;e '
+ 'gratuite du voyage.</p></div>'))
+
+FICHES_B.append(T("p8-1", 8, 1, 2, "Las escapadas &mdash; en train, une heure", "Sagunto &amp; X&agrave;tiva",
+ '<p><span class="lettrine le">D</span>eux ch&acirc;teaux &agrave; port&eacute;e de Cercan&iacute;as. '
+ '<strong>Sagunto</strong> (30&ndash;40 min, ~6,80&nbsp;&euro; A/R)&nbsp;: th&eacute;&acirc;tre romain, '
+ 'quartier juif, et une forteresse-cr&ecirc;te <strong>gratuite</strong> qui d&eacute;roule la '
+ 'M&eacute;diterran&eacute;e. La demi-journ&eacute;e la plus facile.</p>'
+ '<p><strong>X&agrave;tiva</strong> (50&ndash;60 min, ~8&nbsp;&euro; A/R)&nbsp;: ville natale des papes '
+ '<strong>Borgia</strong>, ruelles anciennes, et un ch&acirc;teau double (~6&nbsp;&euro;) accroch&eacute; '
+ '&agrave; sa cr&ecirc;te comme un dragon endormi. Pour le vin&nbsp;: <strong>Requena</strong> et ses '
+ 'caves creus&eacute;es, capitale du c&eacute;page Bobal.</p>'
+ '<div class="merksatz"><p>Gares&nbsp;: <strong>Estaci&oacute; del Nord</strong> (moderniste, centre) '
+ 'pour les r&eacute;gionaux&nbsp;; <strong>Joaqu&iacute;n Sorolla</strong> pour l&rsquo;AVE '
+ '(Madrid en 1h50). Elles sont &agrave; dix minutes &agrave; pied l&rsquo;une de l&rsquo;autre.</p></div>'))
+
+FICHES_B.append(T("p8-2", 8, 2, 2, "Las escapadas &mdash; plus loin, plus &eacute;trange", "P&eacute;&ntilde;iscola &amp; cie",
+ '<p><span class="lettrine le">P</span>our une journ&eacute;e pleine&nbsp;: <strong>Pe&ntilde;&iacute;scola</strong>, '
+ '&laquo;&nbsp;ville dans la mer&nbsp;&raquo; du pape Luna &mdash; et Meereen dans <em>Game of '
+ 'Thrones</em> (~1h20&ndash;2h, plus simple en bus ou tour). Les <strong>grottes de San Jos&eacute;</strong> '
+ '(Vall d&rsquo;Uix&oacute;) cachent la plus longue rivi&egrave;re souterraine <strong>navigable</strong> '
+ 'd&rsquo;Europe&nbsp;: on la visite en barque, sous la roche.</p>'
+ '<p><strong>Montanejos</strong>&nbsp;: sources thermales &agrave; 25&nbsp;&deg;C dans un canyon. '
+ '<strong>Chulilla</strong>&nbsp;: ponts suspendus au-dessus des gorges du Turia (rando ~4h, voiture '
+ 'ou tour conseill&eacute;s). <strong>Gandia</strong>&nbsp;: palais des Borgia, grande plage, et la '
+ 'patrie de la fideu&agrave;.</p>'
+ '<div class="achtung"><p>Le dernier mercredi d&rsquo;ao&ucirc;t, Bu&ntilde;ol (~40 min) devient la plus '
+ 'grande bataille de tomates du monde&nbsp;: <strong>La Tomatina</strong>. Billet obligatoire, '
+ 'v&ecirc;tements sacrifiables.</p></div>'))
+
+FICHES_B.append(T("p9-1", 9, 1, 2, "En camino &mdash; bouger sans Uber", "m&eacute;tro, v&eacute;lo, Cabify",
+ '<p><span class="lettrine le">D</span>epuis l&rsquo;a&eacute;roport&nbsp;: m&eacute;tro lignes '
+ '<strong>3/5</strong> (~4,80&nbsp;&euro;, 20&ndash;25 min) ou taxi &agrave; tarif encadr&eacute; '
+ '(~20&ndash;25&nbsp;&euro;). Astuce de wagon l&eacute;ger&nbsp;: la station <em>Roses</em> est &agrave; '
+ '11 minutes &agrave; pied du terminal et rel&egrave;ve du tarif ville normal (1,50&nbsp;&euro;).</p>'
+ '<p>En ville&nbsp;: la carte <strong>SUMA 10</strong> (~8&nbsp;&euro;, dix trajets m&eacute;tro-tram-bus) '
+ 'est imbattable. <strong>Uber n&rsquo;op&egrave;re pas &agrave; Valence</strong>&nbsp;: installe '
+ '<strong>Cabify</strong> (le choix local), Bolt ou FreeNow. Et Valence est <em>plate</em>&nbsp;: '
+ '<strong>Valenbisi</strong> ou un loueur, puis les 9&nbsp;km du Turia &agrave; deux roues.</p>'
+ '<div class="achtung"><p>Valide <strong>toujours</strong> ta carte au portique, m&ecirc;me s&rsquo;il '
+ 'ne bipe pas&nbsp;: le contr&ocirc;le tombe &agrave; ~50&nbsp;&euro;, et l&rsquo;innocence ne se plaide pas.</p></div>'))
+
+FICHES_B.append(T("p9-2", 9, 2, 2, "En camino &mdash; l&rsquo;argent &amp; les mains baladeuses", "d&eacute;jouer",
+ '<p><span class="lettrine le">S</span>ans contact partout, un peu d&rsquo;esp&egrave;ces pour les '
+ 'march&eacute;s. Deux r&eacute;flexes bancaires&nbsp;: &eacute;viter les distributeurs '
+ '<strong>Euronet</strong> (frais gonfl&eacute;s), et <strong>toujours refuser la conversion '
+ 'dynamique</strong> &mdash; payer en <strong>EUR</strong>, jamais en dollars canadiens propos&eacute;s '
+ 'par la machine. Hors-UE, garde tes factures&nbsp;: le <em>tax free</em> se fait viser &agrave; '
+ 'l&rsquo;a&eacute;roport avant l&rsquo;enregistrement.</p>'
+ '<p>Valence est l&rsquo;une des grandes villes les plus s&ucirc;res d&rsquo;Espagne&nbsp;: le seul vrai '
+ 'risque est le <strong>pickpocket</strong> &mdash; Mercado Central, m&eacute;tro bond&eacute;, plages '
+ '(rien sans surveillance pendant la baignade), foules des Fallas. Classiques &agrave; d&eacute;jouer&nbsp;: '
+ 'la tache &laquo;&nbsp;accidentelle&nbsp;&raquo; qu&rsquo;on t&rsquo;essuie, le bracelet de romarin '
+ 'forc&eacute;, la p&eacute;tition &agrave; planchette, le bonneteau.</p>'
+ '<div class="merksatz"><p>Urgences&nbsp;: <strong>112</strong>, en anglais. Prises type C/F 230&nbsp;V '
+ '(adaptateur depuis le Canada), indicatif <strong>+34</strong>, six heures de plus qu&rsquo;au '
+ 'Qu&eacute;bec. Meilleures fen&ecirc;tres&nbsp;: mai&ndash;juin et septembre&ndash;octobre.</p></div>'))
+
+# ---------- LA DESPEDIDA (§11) ----------
+DESPEDIDA = T("p11-1", 11, 1, 1, "La despedida &mdash; le llibret qu&rsquo;on ne br&ucirc;le pas", "",
+ '<p><span class="lettrine le">U</span>n <em>llibret</em> finit toujours pareil&nbsp;: au feu de la '
+ 'Crem&agrave;, avec le monument qu&rsquo;il racontait. Des mois de travail, vingt minutes de flammes, '
+ 'et les Valenciens applaudissent &mdash; parce que ce qui compte a d&eacute;j&agrave; eu lieu.</p>'
+ '<p>Celui-ci fait exception&nbsp;: garde-le. Reviens-y au comptoir d&rsquo;une horchater&iacute;a, dans '
+ 'le bus d&rsquo;El Palmar, ou la veille de commander ta premi&egrave;re paella. Et si tu ne retiens '
+ 'que trois choses&nbsp;: la paella se mange le <strong>midi</strong>, l&rsquo;horchata se boit '
+ '<strong>sans gla&ccedil;ons</strong>, et <em lang="es">vale</em> r&eacute;pond &agrave; tout.</p>'
+ '<div class="merksatz"><p><em lang="es">Que aproveche, Stef.</em> Le socarrat t&rsquo;attend au fond '
+ 'de la po&ecirc;le.</p></div>')
+
+# ---------- LE KOLOPHON ----------
+KOLOPHON = ('<section class="fiche" id="kolophon"><div class="corps-sec kolophon-corps">'
+ '<h2 class="kolophon-titre">Colof&oacute;n</h2>'
+ '<p class="kolophon-l1">Ce guide a &eacute;t&eacute; cuit dans la vibe <strong>socarrat-llibret</strong>&nbsp;: '
+ 'terracotta de Paterna, safran, cr&egrave;me de riz et vert de huerta. La guitare pince, la noria '
+ 'tourne, la masclet&agrave; tonne.</p>'
+ '<p class="kolophon-l2">Forg&eacute; par <strong>&Eacute;tienne</strong> &amp; '
+ '<strong>Claudi-brimbor</strong>, pour <strong>St&eacute;phanie</strong>, en partance pour Valence '
+ '&mdash; ANNO 2026. <em lang="es">&iexcl;Buen viaje!</em> <em>Bon viatge!</em></p>'
+ '<a class="retour-classeur" href="/">&larr; EL ARCHIVADOR &mdash; retour au classeur</a>'
+ '</div></section>')
+
+# ---------- LA CREMÀ SECRÈTE (geheim) ----------
+GEHEIM = ('<section class="fiche" id="geheim"><div class="corps-sec adyton-corps">'
+ '<span class="stempel-header adyton-stemp">LA CREM&Agrave;</span>'
+ '<h2 class="adyton-titre">Le taller de l&rsquo;artiste fallero</h2>'
+ '<p><span class="lettrine le">T</span>u as pouss&eacute; la porte du <strong lang="es">taller</strong> '
+ '&mdash; l&rsquo;atelier o&ugrave; naissent les g&eacute;ants de mars. Toute l&rsquo;ann&eacute;e, des '
+ 'sculpteurs y mod(&egrave;)lent des visages de trois m&egrave;tres en sachant une seule chose de '
+ 'leur &oelig;uvre&nbsp;: la nuit du 19, elle br&ucirc;lera. C&rsquo;est le pacte de la '
+ '<strong>Crem&agrave;</strong> &mdash; l&rsquo;art valencien ne se conserve pas, il culmine.</p>'
+ '<p>Une seule figurine &eacute;chappe au feu chaque ann&eacute;e&nbsp;: le '
+ '<strong lang="es">ninot indultat</strong>, graci&eacute; par le vote du peuple, qui entre au '
+ 'Museo Fallero pour l&rsquo;&eacute;ternit&eacute;.</p>'
+ '<div class="merksatz adyton-dedicace"><p>Pour toi, St&eacute;phanie&nbsp;: les artistes falleros ont '
+ 'd&eacute;pouill&eacute; le scrutin &mdash; cette ann&eacute;e, le <em>ninot indultat</em>, '
+ 'c&rsquo;est toi. Que Valence te grâcie de tout, et que le voyage te garde du feu&nbsp;'
+ 'juste la chaleur.</p></div>'
+ '<a class="retour-classeur" href="#agora" id="geheim-retour">&larr; Refermer la porte du taller</a>'
+ '</div></section>')
